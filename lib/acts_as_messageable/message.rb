dd0236ee97365654c50473d9fd80359c120110ea
andrzej.kajetanowicz <andrzej.kajetanowicz@gmail.com>
2011-04-11 11:05:21 +0200

    removed overengineered code

--- lib/acts_as_messageable/message.rb
+++ lib/acts_as_messageable/message.rb
@@ -13,20 +13,17 @@ module ActsAsMessageable
 
     validates_presence_of :topic ,:body
 
-    def open?
-      self.opened?
-    end
-
+    
     def open
       self.opened = true
     end
 
     def from
-      "#{self.sent_messageable_type}".constantize.find(self.sent_messageable_id)
+      sent_messageable
     end
 
     def to
-      "#{self.received_messageable_type}".constantize.find(self.received_messageable_id)
+      received_messageable
     end
 
   end
